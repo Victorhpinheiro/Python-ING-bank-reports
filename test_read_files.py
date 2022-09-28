6aa8922367432f556388d56c59fe5a2eea64530c
victorhpinheiro <engvictorpinheiro@gmail.com>
2022-09-28 09:36:17 -0300

    preparing classes and row

--- test_read_files.py
+++ test_read_files.py
@@ -1,1 +1,21 @@
-unittest 
+import unittest
+import read_files
+
+class Test_row(unittest.TestCase):
+    
+    def test_row(self):
+        name = ['row1', 'row2']
+        date = ['12/05/2022', '15/05/2022']
+        description = ['Salary', 'Other']
+        credit = [2000, 0]
+        debit = [0, -430]
+        balance =[ 500, 500]
+
+        for i in range(2):
+            
+
+class Test_read_csv(unittest.TestCase):
+
+class Test_format_Date(unittest.TestCase):
+
+# Tests for read_files.py
